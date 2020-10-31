aec03ac0b6c2149421496411c7cce52e13eeaa8f
Dimitar Micevski <dmicevski@hotmail.com>
2020-10-31 10:59:58 -0400

    created header, game description, & start btn in html file, & created variables & functions in js file

---
+++ script.js
@@ -0,0 +1,23 @@
+var startBtn = document.getElementById("start");
+
+var questions = ["text"];
+var answers = ["text"];
+var timer;
+var questionEl = document.getElementById("question");
+var headerEl = document.getElementById("questionHeader");
+
+function onStart() {
+    // This function needs to start the timer and add text to questions
+    // & answers and hide button and reveal question. 
+    timer = setInterval(clockTick, 1000);
+    questionEl.textContent = questions[0];
+    
+}
+
+// function clocktick will deduct 1 second from inital timer
+
+function clockTick() {
+
+}
+
+startBtn.addEventListener("click", onStart);
