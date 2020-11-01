868438139eea3a70272a2eb2064e8aa03dc15700
Dimitar Micevski <dmicevski@hotmail.com>
2020-10-31 22:59:52 -0400

    removed old function

--- script.js
+++ script.js
@@ -1,21 +1,10 @@
-// var startBtn = document.getElementById("start");
-
 var questions = ["text"];
 var answers = ["text"];
-// var timer = 30;
 var questionEl = document.getElementById("question");
 var headerEl = document.getElementById("questionHeader");
-// var clock = document.getElementById("countdown");
 
-// function onStart() {
-//     // This function needs to start the timer and add text to questions
-//     // & answers and hide button and reveal question. 
-//     timer = setInterval(clockTick, 1000);
-//     questionEl.textContent = questions[0];
-    
-// }
-
-// function clocktick will deduct 1 second from inital timer
+// This function needs to start the timer and add text to questions
+// & answers and hide button and reveal question. 
 
 document.getElementById("start").addEventListener("click", function() {
     var timer = 30;
@@ -31,5 +20,3 @@ document.getElementById("start").addEventListener("click", function() {
         timer -= 1;
 }, 1000);
 });
-
-// startBtn.addEventListener("click", clockTick)
