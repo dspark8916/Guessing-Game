20802ce9f4b4ebf97fad825c59451628929e5b51
Dimitar Micevski <dmicevski@hotmail.com>
2020-10-31 22:58:02 -0400

    created new function to start timer

--- script.js
+++ script.js
@@ -1,24 +1,35 @@
-var startBtn = document.getElementById("start");
+// var startBtn = document.getElementById("start");
 
 var questions = ["text"];
 var answers = ["text"];
-var timer;
+// var timer = 30;
 var questionEl = document.getElementById("question");
 var headerEl = document.getElementById("questionHeader");
-var clock = document.getElementById("countdown");
+// var clock = document.getElementById("countdown");
 
-function onStart() {
-    // This function needs to start the timer and add text to questions
-    // & answers and hide button and reveal question. 
-    timer = setInterval(clockTick, 1000);
-    questionEl.textContent = questions[0];
+// function onStart() {
+//     // This function needs to start the timer and add text to questions
+//     // & answers and hide button and reveal question. 
+//     timer = setInterval(clockTick, 1000);
+//     questionEl.textContent = questions[0];
     
-}
+// }
 
 // function clocktick will deduct 1 second from inital timer
 
-function clockTick() {
+document.getElementById("start").addEventListener("click", function() {
+    var timer = 30;
 
-}
+    var clockTick = setInterval(function() {
+        if(timer <= 0) {
+            clearInterval(clockTick);
+            document.getElementById("countdown").innerHTML = "Time's Up!!";
+        }
+        else {
+            document.getElementById("countdown").innerHTML = timer + " seconds left!!";
+        }
+        timer -= 1;
+}, 1000);
+});
 
-startBtn.addEventListener("click", onStart);
+// startBtn.addEventListener("click", clockTick)
