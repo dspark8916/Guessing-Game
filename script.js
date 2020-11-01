5af3938582f64a1ea117c202c1ff3bfd97735fc3
Dimitar Micevski <dmicevski@hotmail.com>
2020-10-31 23:37:27 -0400

    created an array for the 5 questions

--- script.js
+++ script.js
@@ -1,4 +1,55 @@
-var questions = ["text"];
+var questions = [ 
+    {
+        question: "What is 2 + 2?",
+        answer: {
+            a: "6",
+            b: "2",
+            c: "4",
+            d: "5"
+        },
+        correctAnswer: "c"
+    },
+    {
+        question: "What color is the sky?",
+        answer: {
+            a: "red",
+            b: "yellow",
+            c: "green",
+            d: "blue",
+        },
+        correctAnswer: "d"
+    },
+    {
+        question: "How many inches in a foot?",
+        answer: {
+            a: "12",
+            b: "10",
+            c: "14",
+            d: "16",
+        },
+        correctAnswer: "a"
+    },
+    {
+        question: "How many letters in the alphabet?",
+        answer: {
+            a: "32",
+            b: "26",
+            c: "24",
+            d: "28",
+        },
+        correctAnswer: "b"
+    },
+    {
+        question: "What sport does not use a round ball?",
+        answer: {
+            a: "Baseball",
+            b: "Soccer",
+            c: "Football",
+            d: "Basketball",
+        },
+        correctAnswer: "c",
+    },
+];
 var answers = ["text"];
 var questionEl = document.getElementById("question");
 var headerEl = document.getElementById("questionHeader");
